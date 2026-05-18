1e33f3d30771f0f12cc7da883d2a298e9c28efbd
Ian Alejandro Rivera Torres <20243ds030@utez.edu.mx>
2026-05-18 13:24:27 -0600

    first commit

---
+++ src/main/java/mx/edu/cenidet/estadias/EstadiasApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.cenidet.estadias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/cenidet/estadias/EstadiasApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.cenidet.estadias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstadiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
